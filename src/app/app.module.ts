55b23cd4aaa8160e45cd4bdbe7b680611a44f3d9
Davila Laura Carlos Fabricio <davila.013car013@gmail.com>
2021-06-22 10:16:42 -0400

    Inicializando el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TableroComponent } from './components/tablero/tablero.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { EditarClienteComponent } from './components/editar-cliente/editar-cliente.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { ConfiguracionComponent } from './components/configuracion/configuracion.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TableroComponent,
+    ClientesComponent,
+    EditarClienteComponent,
+    LoginComponent,
+    RegistroComponent,
+    ConfiguracionComponent,
+    NotFoundComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
