0b131be6c08975197bf6c90598640e530eb254d0
Davila Laura Carlos Fabricio <davila.013car013@gmail.com>
2021-06-22 12:14:55 -0400

    Modulos para firestore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firestore: {
+    apiKey: "AIzaSyDAyspmNav4YcVYbU80jktTxlx1-qaijm8",
+    authDomain: "control-clientes-1de31.firebaseapp.com",
+    projectId: "control-clientes-1de31",
+    storageBucket: "control-clientes-1de31.appspot.com",
+    messagingSenderId: "569659092971",
+    appId: "1:569659092971:web:5d70d3a1cbaa12a073f3ae",
+    measurementId: "G-PRYRBEMZ5L"
+  }
 };
